a29ba5ce593b893d5bd38fb832e2855c4eb07f67
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-31 23:53:58 -0400

    Mostrando los juegos y usando informacion en cache para reducir las peticiones

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,16 +2,28 @@ import { Game } from './../interfaces/interfaces';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-
+  juegos:Game[]=[];
   constructor(private http:HttpClient) { }
   
   getNominados(){
-    return this.http.get<Game[]>(`${environment.url}/api/goty`);
+    if(this.juegos.length===0){
+      console.log('Desde internet');
+      
+      return this.http.get<Game[]>(`${environment.url}/api/goty`)
+      .pipe(
+        tap(resp=>this.juegos=resp)
+      );
+
+    }else{
+      console.log('Desde cache');
+      return of(this.juegos);
+    }
   }
 
 }
